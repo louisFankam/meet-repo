61ee746ec1c9602c1db3a190af1f9d25fb81296d
louisfankam <dominiquelouis15@gmail.com>
2025-11-11 16:07:59 +0000

    db

--- migrate_database.py
+++ migrate_database.py
@@ -15,7 +15,7 @@ def get_db_connection():
     return pymysql.connect(
         host=os.getenv('DB_HOST', 'localhost'),
         user=os.getenv('DB_USER', 'root'),
-        password=os.getenv('DB_PASSWORD', 'root'),
+        password=os.getenv('DB_PASSWORD', 'dbsecret'),
         database=os.getenv('DB_NAME', 'meet_db'),
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
